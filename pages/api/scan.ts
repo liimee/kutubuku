ffcf31905c6d2d4ab227bd5b0ad3cd5ba1b0a61d
ta <alt3753.7@gmail.com>
2023-01-25 20:32:42 +0700

    protect /api/scan

--- pages/api/scan.ts
+++ pages/api/scan.ts
@@ -1,8 +1,16 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import scan from '../../scan';
+import { authOptions } from "./auth/[...nextauth]"
+import { unstable_getServerSession } from "next-auth/next"
 
-export default function handler(_: NextApiRequest, res: NextApiResponse) {
-  scan();
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const session = await unstable_getServerSession(req, res, authOptions)
 
-  res.status(200).json({ message: '(hopefully) ok' })
+  if (session) {
+    scan();
+
+    res.status(200).json({ message: '(hopefully) ok' });
+  } else {
+    res.status(401).send('Unauthorized :)');
+  }
 }

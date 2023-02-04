4bb69e74610677bdb0146020ae0dc3f513ea5a45
ta <alt3753.7@gmail.com>
2023-02-04 09:29:18 +0700

    use standard img

--- utils/bookthumb.tsx
+++ utils/bookthumb.tsx
@@ -1,6 +1,6 @@
+/* eslint-disable @next/next/no-img-element */
 /* eslint-disable jsx-a11y/alt-text */
-import Image from "next/image";
 
 export default function BookThumb(props: any) {
-  return <Image width={150} height={260} {...props} src={'/thumbnails/' + props.id + '.jpg'} />
+  return <img {...props} src={'/thumbnails/' + props.id + '.jpg'} />
 }

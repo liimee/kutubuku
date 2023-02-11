b4e412c8f61d92682c5fd89a7aeaab47c55dcae7
ta <alt3753.7@gmail.com>
2023-02-11 13:30:08 +0700

    styling stuff

--- utils/bookgrid.tsx
+++ utils/bookgrid.tsx
@@ -5,12 +5,14 @@ import BookThumb from "./bookthumb";
 
 export default function BookGrid({ list }: { list: Book[] | null }) {
   return (
-    <Grid container spacing={2}>
+    <Grid container spacing={2} justifyContent='space-evenly'>
       {
-        list ? list.map((v: { id: string }) =>
+        list ? list.map((v: {
+          title: string; id: string
+        }) =>
           <Grid item key={v.id}>
             <Button color='secondary' sx={{ p: 0 }} LinkComponent={Link} href={"/books/" + v.id} variant="text">
-              <BookThumb style={{ borderRadius: '4px', height: '14rem' }} id={v.id} alt="" />
+              <BookThumb style={{ borderRadius: '4px', height: '14rem' }} id={v.id} alt={v.title} />
             </Button>
           </Grid>
         ) : <Grid item><CircularProgress /></Grid>
